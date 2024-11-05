de6a99a95f78adf78f8bea684df58ce24178a9d9
victorFlorentino12 <florentinoalmeidavc@outlook.com>
2024-11-04 22:55:20 -0300

    update project for train typescript

---
+++ src/A0001-type-annotation/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const nome: string = 'oi';
+const idade: number = 3
+const adult: boolean = true
+const simbolo: symbol = Symbol('valor')
+
+
+//array
+let arrayNumber: Array<number> = [1,2,3]
+let arrayString: Array<string> = ['1','2','3']
+let arrayNumber2: number[] = [1,2,3]
